67822d15fce2a11dce82533522d924c1e47a432a
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2022-04-05 01:22:56 +0200

    Add templated index

--- products/views.py
+++ products/views.py
@@ -8,8 +8,12 @@ from products.models import Product
 
 
 def index(request):
-    products = Product.objects.all()
     template = loader.get_template('products/index.html')
+    return HttpResponse(template.render({}, request))
+
+def product(request):
+    products = Product.objects.all()
+    template = loader.get_template('products/product.html')
     context = {
         'products_list': products,
     }
