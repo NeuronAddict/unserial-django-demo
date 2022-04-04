c0deeb2fec525504038670728ddb55305b779550
neuronaddict <manufrommars@elacourarie.org>
2022-04-05 01:22:56 +0200

    Add templated index

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from products import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product/', views.product, name='product'),
     path('<int:product_id>/', views.detail, name='detail'),
 ]
